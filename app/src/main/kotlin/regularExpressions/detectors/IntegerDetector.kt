3e092c668e3c14adddbf434f2da741e031e8993e
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-08-07 20:45:55 -0600

    added Integer Detector

--- app/src/main/kotlin/regularExpressions/detectors/IntegerDetector.kt
+++ app/src/main/kotlin/regularExpressions/detectors/IntegerDetector.kt
@@ -1,9 +1,29 @@
 package regularExpressions.detectors
 
 import regularExpressions.Detector
+import regularExpressions.State
 
 class IntegerDetector: Detector {
+    private var state: State = Start
+
     override fun accepts(input: String): Boolean {
-        TODO("Not yet implemented")
+        if (input.isEmpty()) return false
+        state = Start
+        for (char in input) {
+            state = state.on(char.toString()) ?: return false
+        }
+        return state.canAccept()
+    }
+
+    private object Start: State {
+        override fun on(char: String): State? = if (char in "1".."9") Digits else null
+
+        override fun canAccept(): Boolean = false
+    }
+
+    private object Digits: State {
+        override fun on(char: String): State? = if (char in "0".."9") Digits else null
+
+        override fun canAccept(): Boolean = true
     }
 }
