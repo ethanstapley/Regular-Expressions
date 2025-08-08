109be5784a0d84117ae766788a8ec0de1a84095b
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-08-07 21:03:10 -0600

    added Binary Detector

--- app/src/main/kotlin/regularExpressions/detectors/BinaryDetector.kt
+++ app/src/main/kotlin/regularExpressions/detectors/BinaryDetector.kt
@@ -1,9 +1,48 @@
 package regularExpressions.detectors
 
 import regularExpressions.Detector
+import regularExpressions.State
 
 class BinaryDetector: Detector {
+    private var state: State = BinaryStart
+
     override fun accepts(input: String): Boolean {
-        TODO("Not yet implemented")
+        if (input.isEmpty()) return false
+        state = BinaryStart
+        for (char in input) {
+            state = state.on(char.toString()) ?: return false
+        }
+        return state.canAccept()
     }
+
+    private object BinaryStart: State {
+        override fun on(char: String): State? = if (char == "1") BinaryIsOne else null
+
+        override fun canAccept(): Boolean = false
+    }
+
+    private object BinaryIsOne: State {
+        override fun on(char: String): State? {
+            return when (char) {
+                "1" -> BinaryIsOne
+                "0" -> BinaryIsZero
+                else -> null
+            }
+        }
+
+        override fun canAccept(): Boolean = true
+    }
+
+    private object BinaryIsZero: State {
+        override fun on(char: String): State? {
+            return when (char) {
+                "1" -> BinaryIsOne
+                "0" -> BinaryIsZero
+                else -> null
+            }
+        }
+
+        override fun canAccept(): Boolean = false
+    }
+
 }
--- app/src/main/kotlin/regularExpressions/detectors/IntegerDetector.kt
+++ app/src/main/kotlin/regularExpressions/detectors/IntegerDetector.kt
@@ -4,25 +4,25 @@ import regularExpressions.Detector
 import regularExpressions.State
 
 class IntegerDetector: Detector {
-    private var state: State = Start
+    private var state: State = IntegerStart
 
     override fun accepts(input: String): Boolean {
         if (input.isEmpty()) return false
-        state = Start
+        state = IntegerStart
         for (char in input) {
             state = state.on(char.toString()) ?: return false
         }
         return state.canAccept()
     }
 
-    private object Start: State {
-        override fun on(char: String): State? = if (char in "1".."9") Digits else null
+    private object IntegerStart: State {
+        override fun on(char: String): State? = if (char in "1".."9") IntegerDigits else null
 
         override fun canAccept(): Boolean = false
     }
 
-    private object Digits: State {
-        override fun on(char: String): State? = if (char in "0".."9") Digits else null
+    private object IntegerDigits: State {
+        override fun on(char: String): State? = if (char in "0".."9") IntegerDigits else null
 
         override fun canAccept(): Boolean = true
     }
