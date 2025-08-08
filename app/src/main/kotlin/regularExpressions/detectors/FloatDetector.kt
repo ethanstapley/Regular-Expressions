f7d5677d51d75b84f152ebcecd497adbf9a068f9
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-08-07 21:30:18 -0600

    made the float Detector

--- app/src/main/kotlin/regularExpressions/detectors/FloatDetector.kt
+++ app/src/main/kotlin/regularExpressions/detectors/FloatDetector.kt
@@ -1,9 +1,61 @@
 package regularExpressions.detectors
 
 import regularExpressions.Detector
+import regularExpressions.State
 
 class FloatDetector: Detector {
+    private var state: State = FloatStart
+
     override fun accepts(input: String): Boolean {
-        TODO("Not yet implemented")
+        if (input.isEmpty()) return false
+        state = FloatStart
+        for (char in input) {
+            state = state.on(char.toString()) ?: return false
+        }
+        return state.canAccept()
     }
+
+    private object FloatStart: State {
+        override fun on(char: String): State? {
+            return when (char) {
+                in "1".."9" -> FloatIntDigits
+                "0" -> FloatZero
+                "." -> FloatDot
+                else -> null
+            }
+        }
+
+        override fun canAccept(): Boolean = false
+    }
+
+    private object FloatIntDigits: State {
+        override fun on(char: String): State? {
+            return when (char) {
+                in "0".."9" -> FloatIntDigits
+                "." -> FloatDot
+                else -> null
+            }
+        }
+
+        override fun canAccept(): Boolean = false
+    }
+
+    private object FloatZero: State {
+        override fun on(char: String): State? = if (char == ".") FloatDot else null
+
+        override fun canAccept(): Boolean = false
+    }
+
+    private object FloatDot: State {
+        override fun on(char: String): State? = if (char in "0".."9") FloatDecimal else null
+
+        override fun canAccept(): Boolean = false
+    }
+
+    private object FloatDecimal: State {
+        override fun on(char: String): State? = if (char in "0".."9") FloatDecimal else null
+
+        override fun canAccept(): Boolean = true
+    }
+
 }
